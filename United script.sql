d2925e91677e40332872c38db604b141bdc79a91
JGamboaB <62961473+JGamboaB@users.noreply.github.com>
2021-05-06 13:54:40 -0600

    Updated database

--- United script.sql
+++ United script.sql
@@ -1,11 +1,9 @@
--- Campos de email y username deben ser únicos para users.
-ALTER TABLE wk_users
-	MODIFY username VARCHAR(20) NOT NULL UNIQUE,
-    MODIFY email VARCHAR(250) NOT NULL UNIQUE;
+use MyWorkouts;
 
 -- Datos para wk_days
 INSERT INTO wk_days (`name`)
-VALUES ('Sunday'), ('Monday'), ('Tuesday'),('Wednesday'),('Thursday'),('Friday'),('Saturday');
+VALUES ('Sunday'), ('Monday'), ('Tuesday'),
+('Wednesday'),('Thursday'),('Friday'),('Saturday');
 
 -- Datos para wk_paymentStatus
 INSERT INTO wk_paymentStatus(name)
@@ -36,48 +34,43 @@ of the app.'),
 (4, 'Manager', 'Role in charge of the management and general supervision of the app, has 
 all permits.');
 
+-- Por ahora solo tenemos la app principal
+INSERT INTO wk_applications (name, description)
+VALUES
+('My Workout', 'Main app aimed at common users where they can create and follow 
+their own workouts.');
+
+-- Module TEMPORAL para poder usar los permisos, CAMBIAR cuando se pueda
+INSERT INTO wk_modules (`name`, applicationid)
+VALUES ('Temporary', 1);
+
 -- Datos de wk_permissions. Permisos de empleados
-INSERT INTO wk_permissions (permissionid, moduleid, `name`, description, `code`, 
-enabled, deleted)
+INSERT INTO wk_permissions (moduleid, `name`, description, `code`)
 VALUES 
-(1, 1, 'Chat', '.', 501, 1, 0),
-(2, 1, 'Supervise Chats', 'Permission to use chat.', 502, 1, 0),
-(3, 1, 'Preloaded Exercises', 'Permission to edit preloaded exercises.', 503, 1, 0),
-(4, 1, 'Transactions', 'Permission to view user transactions.', 504, 1, 0),
-(5, 1, 'Vault', 'Permission to review money inflows and outflows from the app.', 505, 1, 0),
-(6, 1, 'Admin', 'Permission to edit roles, permissions, and user accounts.', 506, 1, 0),
-(7, 1, 'Money', 'Permission to handle the money in the app.', 507, 1, 0);
+(1, 'Chat', '.', 501),
+(1, 'Supervise Chats', 'Permission to use chat.', 502),
+(1, 'Preloaded Exercises', 'Permission to edit preloaded exercises.', 503),
+(1, 'Transactions', 'Permission to view user transactions.', 504),
+(1, 'Vault', 'Permission to review money inflows and outflows from the app.', 505),
+(1, 'Admin', 'Permission to edit roles, permissions, and user accounts.', 506),
+(1, 'Money', 'Permission to handle the money in the app.', 507);
 
 
 -- Es un historial, por lo que para inciar van como editados por "Managment" con id = 0
--- Checksum se llena con stored procedures
-INSERT INTO wk_permissonsPerRole (permissionid, roleid, editedby, editorid, enabled, lastupdate, `checksum`)
+INSERT INTO wk_permissionsPerRole (permissionid, roleid, editedby, editorid, enabled, lastupdate, `checksum`)
 VALUES 
-(1, 1, 'Managment', 0, 1, NOW(), ' '),
-(3, 1, 'Managment', 0, 1, NOW(), ' '),
-(1, 2, 'Managment', 0, 1, NOW(), ' '),
-(4, 3, 'Managment', 0, 1, NOW(), ' '),
-(5, 3, 'Managment', 0, 1, NOW(), ' '),
-(1, 4, 'Managment', 0, 1, NOW(), ' '),
-(2, 4, 'Managment', 0, 1, NOW(), ' '),
-(3, 4, 'Managment', 0, 1, NOW(), ' '),
-(4, 4, 'Managment', 0, 1, NOW(), ' '),
-(5, 4, 'Managment', 0, 1, NOW(), ' '),
-(6, 4, 'Managment', 0, 1, NOW(), ' '),
-(7, 4, 'Managment', 0, 1, NOW(), ' ');
-
-
--- Module TEMPORAL para poder usar los permisos, CAMBIAR cuando se pueda
-INSERT INTO wk_modules (moduleid, name, applicationid)
-VALUES
-(1, 'Temporary', 1);
-
-
--- Por ahora solo tenemos la app principal
-INSERT INTO wk_applications (applicationid, name, description)
-VALUES
-(1, 'My Workout', 'Main app aimed at common users where they can create and follow 
-their own workouts.');
+(1, 1, 'Managment', 0, 1, NOW(), sha2(concat(permissionid,roleid,editedby,editorid,lastupdate),256)),
+(3, 1, 'Managment', 0, 1, NOW(), sha2(concat(permissionid,roleid,editedby,editorid,lastupdate),256)),
+(1, 2, 'Managment', 0, 1, NOW(), sha2(concat(permissionid,roleid,editedby,editorid,lastupdate),256)),
+(4, 3, 'Managment', 0, 1, NOW(), sha2(concat(permissionid,roleid,editedby,editorid,lastupdate),256)),
+(5, 3, 'Managment', 0, 1, NOW(), sha2(concat(permissionid,roleid,editedby,editorid,lastupdate),256)),
+(1, 4, 'Managment', 0, 1, NOW(), sha2(concat(permissionid,roleid,editedby,editorid,lastupdate),256)),
+(2, 4, 'Managment', 0, 1, NOW(), sha2(concat(permissionid,roleid,editedby,editorid,lastupdate),256)),
+(3, 4, 'Managment', 0, 1, NOW(), sha2(concat(permissionid,roleid,editedby,editorid,lastupdate),256)),
+(4, 4, 'Managment', 0, 1, NOW(), sha2(concat(permissionid,roleid,editedby,editorid,lastupdate),256)),
+(5, 4, 'Managment', 0, 1, NOW(), sha2(concat(permissionid,roleid,editedby,editorid,lastupdate),256)),
+(6, 4, 'Managment', 0, 1, NOW(), sha2(concat(permissionid,roleid,editedby,editorid,lastupdate),256)),
+(7, 4, 'Managment', 0, 1, NOW(), sha2(concat(permissionid,roleid,editedby,editorid,lastupdate),256));
 
 
 -- Datos de wk_ticketCategories
@@ -87,7 +80,6 @@ VALUES
 ('Subscription'),
 ('User account'),
 ('Cancelation');
-
 
 -- Datos de wk_ticketStatus
 INSERT INTO wk_ticketStatus (name)
@@ -104,54 +96,48 @@ VALUES
 ('Low'),
 ('Medium'),
 ('High'),
-('Critical');
+('Urgent');
 
 -- Datos de wk_chatStatus
 INSERT INTO wk_chatStatus (name)
 VALUES
 ('Online'),
-('Away'),
+('Outside'),
 ('Occupied'),
-('Offline');
+('Disconnected');
 
--- Datos de wk_departments. Employees trabajan en departamentos.
+-- Datos de wk_departments
 INSERT INTO wk_departments (name)
 VALUES
 ('Development'), ('Customer Service'), ('Marketing'),
 ('Financial Accounting'), ('Human Resources'), ('Management');
 
 -- Datos para wk_categories. Categorías de wk_exercises
-INSERT INTO wk_categories(`name`, `description`)
-VALUES ('Endurance', 'Ability of a given muscle to exert force, consistently and repetitively, over a period of time.'),
+insert into wk_categories(`name`, `description`)
+values ('Endurance', 'Ability of a given muscle to exert force, consistently and repetitively, over a period of time.'),
 ('Strength', 'Focus on strength of a specific body part'),
 ('Hypertrophy', 'Increase and growth of muscle cells'),
 ('Cardio', 'Gets your heart rate up and keeps it up for a prolonged period of time.'); 
 
 -- Datos para wk_difficultyLevels 
-INSERT INTO wk_difficultyLevels(`name`)
-VALUES ('Beginner'), ('Intermediate'), ('Expert');
+insert into wk_difficultyLevels(`name`)
+values ('Beginner'), ('Intermediate'), ('Expert');
 
 -- Datos para wk_exerciseFocus
-INSERT INTO wk_exerciseFocus(`name`)
-values ('Biceps'), ('Triceps'), 
-('Lats'), ('Back'), 
-('Chest'), ('Legs'), 
-('Thighs'), ('Forearms'), 
-('Calves'), ('Deltoids'), ('Abs');
+insert into wk_exerciseFocus(`name`)
+values ('Biceps'), ('Triceps'), ('Lats'), ('Back'), ('Chest'), ('Legs'), ('Thighs'), ('Forearms'), ('Calves'), ('Deltoids'), ('Abs');
 
 -- Datos para wk_equipment
-INSERT INTO wk_equipment(`name`,`description`,weight)
-VALUES 
-('Dumbbells','A normal dumbbell (shape can vary)', 15),
+insert into wk_equipment(`name`,`description`,weight)
+values ('Dumbbells','A normal dumbbell (shape can vary)', 15),
 ('Stationary Bike','Exercise bike', 0),
 ('Exercise Ball','Plastic ball made for exercising', 5),
 ('Jump Rope','Plastic rope made for exercising', 1),
 ('Kettlebell', 'Cast iron/steel ball with a handle attached.',15);
 
 -- Datos para wk_exercises
-INSERT INTO wk_exercises(`name`, categoryid, focusid, difficultyid, equipmentid, steps, `description`, recommendedReps, recommendedSets, recommendedDuration)
-VALUES 
-('Lunges', 4, 7, 1, null, 'Stand with your feet shoulder-width apart and arms down at your sides. 
+insert into wk_exercises(`name`, categoryid, focusid, difficultyid, equipmentid, steps, `description`, recommendedReps, recommendedSets, recommendedDuration)
+values ('Lunges', 4, 7, 1, null, 'Stand with your feet shoulder-width apart and arms down at your sides. 
 Take a step forward with your right leg and bend your right knee, stop when your thigh is parallel to the ground. 
 Return to starting position. Repeat with left leg.', 'Move your torso up and down with your legs', 20, 4, null),
 ('Pushups', 1, 11, 1, null, 'Start in a plank position. Core should be tight, shoulders pulled down and back, and neck neutral. 
@@ -186,52 +172,42 @@ Jump again as you return to the starting position. Repeat.', 'Jump and feel like
 'Planking with one hand at a time.', 20, 3, null);
 
 -- Datos para wk_videos
-INSERT INTO wk_videos(url,deleted)
-VALUES
-('https://thumbs.gfycat.com/RequiredThinAbalone-mobile.mp4',0),
-('https://thumbs.gfycat.com/FittingIdenticalAtlanticspadefish-mobile.mp4',0),
-('https://thumbs.gfycat.com/AdvancedBestChrysalis-mobile.mp4',0),
-('https://thumbs.gfycat.com/CriminalMealyGar-mobile.mp4',0),
-('https://thumbs.gfycat.com/ClutteredEcstaticFinnishspitz-mobile.mp4',0),
-('https://youtu.be/bgYt7ZgFc9M',0),
-('https://www.youtube.com/watch?v=-F-kADNt2Ik&feature=emb_rel_pause',0),
-('https://hmg-h-cdn.hearstapps.com/videos/chest-press-side-1553016142.mp4',0),
-('https://hmg-h-cdn.hearstapps.com/videos/rw-jess-1st-move-1579794350.mp4',0),
-('https://www.youtube.com/watch?v=I-3r4cl4ahA&ab_channel=LivestrongWoman',0),
-('https://youtu.be/9gDfNpzytLo',0),
-('https://www.youtube.com/watch?v=QOCn3_iOAro&ab_channel=Northeastern',0);
+insert into wk_videos(url)
+values ('https://thumbs.gfycat.com/RequiredThinAbalone-mobile.mp4'),
+('https://thumbs.gfycat.com/FittingIdenticalAtlanticspadefish-mobile.mp4'),
+('https://thumbs.gfycat.com/AdvancedBestChrysalis-mobile.mp4'),
+('https://thumbs.gfycat.com/CriminalMealyGar-mobile.mp4'),
+('https://thumbs.gfycat.com/ClutteredEcstaticFinnishspitz-mobile.mp4'),
+('https://youtu.be/bgYt7ZgFc9M'),
+('https://www.youtube.com/watch?v=-F-kADNt2Ik&feature=emb_rel_pause'),
+('https://hmg-h-cdn.hearstapps.com/videos/chest-press-side-1553016142.mp4'),
+('https://hmg-h-cdn.hearstapps.com/videos/rw-jess-1st-move-1579794350.mp4'),
+('https://www.youtube.com/watch?v=I-3r4cl4ahA&ab_channel=LivestrongWoman'),
+('https://youtu.be/9gDfNpzytLo'),
+('https://www.youtube.com/watch?v=QOCn3_iOAro&ab_channel=Northeastern');
 
 -- Datos para wk_videosPerExercise
-INSERT INTO wk_videosPerExercise(videoid, exerciseid)
-VALUES (1,1),(2,2),(3,3),
-(4,4),(5,5),(6,6),
-(7,7),(8,8),(9,9),
-(10,10),(11,11),(12,12);
+insert into wk_videosPerExercise(videoid, exerciseid)
+values (1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10),(11,11),(12,12);
 
 -- Datos para wk_pictures
-INSERT INTO wk_pictures(url,deleted)
-VALUES 
-('https://julielohre.com/wp-content/uploads/2018/07/AlternatingLunges-1-1024x760.jpg',0),
-('https://cdn.prod.openfit.com/uploads/2017/06/01150749/how-to-get-better-at-push-ups2.jpg',0),
-('https://www.crossfit.com/wp-content/uploads/2019/04/03111458/Bent-Over-Row-Collage-1024x576.png',0),
-('https://cdn.prod.openfit.com/uploads/2019/09/05175233/deadlift.jpg',0),
-('https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F35%2F2020%2F02%2Flow-plank-hold.jpg',0),
-('https://i.pinimg.com/originals/9d/89/b0/9d89b04811556ef4cc26c57aa496b509.png',0),
-('https://hips.hearstapps.com/vidthumb/brightcove/57894083e694aa370d8834ff/thumb_1468612741.png',0),
-('https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/topic_centers/Fitness-Exercise/400x400_7_Top_Chest_Exercises_for_Men_Chest_Press.gif?w=525',0),
-('https://i.pinimg.com/originals/29/6e/fd/296efda0d27153eb5dccb4ff922bb1ca.jpg',0),
-('https://i.pinimg.com/originals/3d/0f/0d/3d0f0d046b6c0d7f917e8c36743dc636.jpg',0),
-('https://www.researchgate.net/profile/Mohammed-Abou-Elmagd/publication/341734848/figure/fig1/AS:896516470362114@1590757591713/Sample-of-Jumping-Jacks-Exercise-4.png',0),
-('https://media1.popsugar-assets.com/files/thumbor/UjRu9n3XhNilWLRUd9G86CD0SNo/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/06/14/784/n/1922729/4b0aa10159417724bd23c1.78911403_Plank-Shoulder-Tap/i/Plank-Shoulder-Tap.jpg',0);
-
+insert into wk_pictures(url)
+values ('https://julielohre.com/wp-content/uploads/2018/07/AlternatingLunges-1-1024x760.jpg'),
+('https://cdn.prod.openfit.com/uploads/2017/06/01150749/how-to-get-better-at-push-ups2.jpg'),
+('https://www.crossfit.com/wp-content/uploads/2019/04/03111458/Bent-Over-Row-Collage-1024x576.png'),
+('https://cdn.prod.openfit.com/uploads/2019/09/05175233/deadlift.jpg'),
+('https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F35%2F2020%2F02%2Flow-plank-hold.jpg'),
+('https://i.pinimg.com/originals/9d/89/b0/9d89b04811556ef4cc26c57aa496b509.png'),
+('https://hips.hearstapps.com/vidthumb/brightcove/57894083e694aa370d8834ff/thumb_1468612741.png'),
+('https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/topic_centers/Fitness-Exercise/400x400_7_Top_Chest_Exercises_for_Men_Chest_Press.gif?w=525'),
+('https://i.pinimg.com/originals/29/6e/fd/296efda0d27153eb5dccb4ff922bb1ca.jpg'),
+('https://i.pinimg.com/originals/3d/0f/0d/3d0f0d046b6c0d7f917e8c36743dc636.jpg'),
+('https://www.researchgate.net/profile/Mohammed-Abou-Elmagd/publication/341734848/figure/fig1/AS:896516470362114@1590757591713/Sample-of-Jumping-Jacks-Exercise-4.png'),
+('https://media1.popsugar-assets.com/files/thumbor/UjRu9n3XhNilWLRUd9G86CD0SNo/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/06/14/784/n/1922729/4b0aa10159417724bd23c1.78911403_Plank-Shoulder-Tap/i/Plank-Shoulder-Tap.jpg');
 
 -- Datos para wk_picturesPerExercise
-INSERT INTO wk_picturesPerExercise(pictureid, exerciseid)
-VALUES (1,1),(2,2),(3,3),
-(4,4),(5,5),(6,6),
-(7,7),(8,8),(9,9),
-(10,10),(11,11),(12,12);
-
+insert into wk_picturesPerExercise(pictureid, exerciseid)
+values (1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10),(11,11),(12,12);
 
 -- Datos para wk_users
 INSERT INTO wk_users (genderid, email, username, password, firstname, lastname, birthdate, registeredAt)
@@ -256,9 +232,8 @@ values
 (1, 'chapoguzman@gmail.com', 'Avocadorable', sha2(concat(email,username,'ElChapoMUSULOS1',birthdate),256), 'JOAQUIN', 'GUZMAN', DATE(FROM_UNIXTIME(RAND() * (63075600 - 978310800) + 1230796800)), date_add(date_add(now(), INTERVAL -1 YEAR), INTERVAL floor(rand()*(9999999-1)+1) SECOND)),
 (1, 'saby78@bartoletti.com', 'FartinLutherKing', sha2(concat(email,username,'zoey101',birthdate),256), 'SEBASTIAN', 'VEGA', DATE(FROM_UNIXTIME(RAND() * (63075600 - 978310800) + 1230796800)), date_add(date_add(now(), INTERVAL -1 YEAR), INTERVAL floor(rand()*(9999999-1)+1) SECOND));
 
-
 -- Se pueden utilizar los procedimientos de abajo
-INSERT INTO wk_employee (userid, departmentid)
+INSERT INTO wk_employees (userid, departmentid)
 values (1,1), (2,2), (3,3), (4,4), (5,5), (6,6);
 
 -- Datos para wk_clients
@@ -266,6 +241,7 @@ INSERT INTO wk_clients (userid, planid)
 values (7,2), (8,2), (9,2), (10,2), (11,2), (12,1), (13,1), (14,1), (15,1), (16,1), (17,1), (18,1), (19,1);
 
 
+-- ///////////////////////////////////////////////////////////////////////
 -- Procedimiento para agregar nuevo user-empleado
 DROP PROCEDURE IF EXISTS addEmployees;
 delimiter //
@@ -291,8 +267,6 @@ BEGIN
 	SET FOREIGN_KEY_CHECKS=1;
 END //
 delimiter ;
-
-
 
 
 -- Procedimiento para agregar nuevo user-cliente
@@ -331,5 +305,6 @@ INSERT INTO wk_tickets (description, date, ticketCategoryid, ticketStatusid, tic
 clientid, employeeid)
 VALUES
 ('Descripción temporal', CURRENT_TIMESTAMP, 2, 3, 2, 3, 2);
-select * from wk_tickets
+
+-- select * from wk_tickets
 
